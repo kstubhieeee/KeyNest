6a17cc82685904bcb6938f21345489588fa482e8
kstubhie <banekaustubh27@gmail.com>
2024-12-19 01:47:48 +0530

    color changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className='bg-slate-800 text-white flex flex-col justify-center items-center w-full fixed bottom-0'>
+        <div className='bg-[#1c1c1c] text-white flex flex-col justify-center items-center w-full fixed bottom-0'>
             <div className="logo font-bold text-white text-2xl">
                 <span className='text-green-500'> &lt;</span>
                 <span>KeyNest</span><span className='text-green-500'>/&gt;</span>
