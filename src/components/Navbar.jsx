29f4b8a92e971ca5bb4e3203fd7c2dce85d16e4c
kstubhie <banekaustubh27@gmail.com>
2024-12-19 01:46:49 +0530

    final commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,7 @@ const Navbar = () => {
 
 
                 </div>
-                <button className='text-white bg-green-700 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1'>
-                    <img className='invert  w-10 p-1' src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="github logo" />
-                    <span className='font-bold px-2'>GitHub</span>
 
-                </button>
             </div>
         </nav>
     )
